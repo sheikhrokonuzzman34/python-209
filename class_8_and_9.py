789a5aeca9fa93950d7c43a978276ba1d7c863a5
Soyebmahir <tssoyeb@gmail.com>
2024-03-17 11:40:24 +0600

    first commit

---
+++ class_8_and_9.py
@@ -0,0 +1,12 @@
+# for x in (car1, boat1, plane1):
+#   x.move()
+
+   
+# Python program to demonstrate in-built poly-
+# morphic functions
+ 
+# len() being used for a string
+# print(len("geeks"))
+ 
+# len() being used for a list
+# print(len([10, 20, 30]))
